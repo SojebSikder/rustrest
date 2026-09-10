75dcb185a80c584e3caba87980408c8eec4e23cd
sojebsikder <sojebsikder@gmail.com>
2026-09-10 16:40:10 +0600

    feat: add icon for windows executable

---
+++ build.rs
@@ -0,0 +1,21 @@
+#[cfg(windows)]
+fn main() {
+    let mut res = winres::WindowsResource::new();
+    res.set_icon("assets/images/app-icon.ico");
+
+    // executable file metadata details in Windows Properties
+    res.set("ProductName", "Rustrest");
+    res.set("FileDescription", "API Testing Platform");
+    res.set("LegalCopyright", "Copyright © 2026");
+    res.set("CompanyName", "sojebsikder");
+
+    if let Err(e) = res.compile() {
+        eprintln!("{}", e);
+        std::process::exit(1);
+    }
+}
+
+#[cfg(not(windows))]
+fn main() {
+    // Non-windows targets ignore this step
+}
